d7398855f2941d08739c4cc019612ba6c435953a
Steffra <krauszbertold@gmail.com>
2022-02-19 21:28:13 +0100

    init pwa

--- src/main.ts
+++ src/main.ts
@@ -3,4 +3,15 @@ import App from './App.vue'
 import router from './router'
 import './assets/css/style.css'
 
+import { registerSW } from 'virtual:pwa-register'
+
+const updateSW = registerSW({
+    onNeedRefresh() {
+        alert('need refresh')
+    },
+    onOfflineReady() {
+        alert('offline ready')
+    },
+})
+
 createApp(App).use(router).mount('#app')
--- vite.config.ts
+++ vite.config.ts
@@ -1,30 +1,43 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-    server: {
-        proxy: {
-            '^/api': {
-                target: 'http://localhost:8000',
-                changeOrigin: true,
-                configure: (proxy) => {
-                    proxy.on('error', function (err, req, res) {
-                        console.log(JSON.stringify(err))
-                        res.writeHead(500, {
-                            'Content-Type': 'text/plain',
-                        })
-
-                        res.end(JSON.stringify(err))
-                    })
-                },
-            },
-        },
-    },
     resolve: {
         alias: {
             '/@': resolve(__dirname, 'src'),
         },
     },
-    plugins: [vue()],
+    plugins: [
+        vue(),
+        VitePWA({
+            registerType: 'autoUpdate',
+            includeAssets: ['favicon.svg', 'favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
+            manifest: {
+                name: 'Name of your app',
+                short_name: 'Short name of your app',
+                description: 'Description of your app',
+                theme_color: '#ffffff',
+                icons: [
+                    {
+                        src: 'android-chrome-192x192.png',
+                        sizes: '192x192',
+                        type: 'image/png',
+                    },
+                    {
+                        src: 'android-chrome-512x512.png',
+                        sizes: '512x512',
+                        type: 'image/png',
+                    },
+                    {
+                        src: 'android-chrome-512x512.png',
+                        sizes: '512x512',
+                        type: 'image/png',
+                        purpose: 'any maskable',
+                    },
+                ],
+            },
+        }),
+    ],
 })
